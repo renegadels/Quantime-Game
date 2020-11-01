f5ac4eb726663c13950f39514d39cebd0e95894f
renegadels <renegadelabstudios@hotmail.com>
2020-11-01 20:07:49 +0530

    main commit

---
+++ resources/app/inject/inject.js
@@ -0,0 +1,8 @@
+var data_tracking =  {
+      platform: "windows",
+      appID:"5JlQQlZvx9LBhjhCGBnh",
+      url:"https://5f9ec139c2863.htmlsave.net/",
+      appname:"Quantime Game"
+    };
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push(data_tracking);(function (w, d, s, l, i) {  w[l] = w[l] || [];  w[l].push({  'gtm.start': new Date().getTime(), event: 'gtm.js', }); let f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? `&l=${l}` : ''; j.async = true; j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`; f.parentNode.insertBefore(j, f); }(window, document, 'script', 'dataLayer', 'GTM-N9XW463'));
